2b2ea10eb0b1825e257df15c90ceee9018c75853
Dean Menzel <deantmenzel@gmail.com>
2021-08-02 14:31:13 +1000

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
+const ais = new Object();
+
 document.addEventListener('readystatechange', (event) => {
   console.log(`ReadyState: ${document.readyState}`);
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log(`DOMContentLoaded`);
-  buildDb()
+  fetchdata('https://deantmenzel.github.io/hello-world/db/index.json', buildDB);
 });
 
 window.addEventListener('load', (event) => {
   console.log(`WindowLoaded`);
 });
 
-const buildDb = () => {
+var fetchdata = (url, action) => {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState !== 4) return;
-    console.log(xhr);
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log(JSON.parse(xhr.responseText));
+      action(JSON.parse(xhr.responseText));
     }
   };
-  xhr.open('GET', '../db/index.json');
+  xhr.open('GET', url);
   xhr.send();
+}
+
+var buildDB = (jsondata) => {
+  ais["tradingdays"] = jsondata["tradingdays"];
+  ais["tradingdays"].forEach((tradingday) => {
+    fetchdata(`https://deantmenzel.github.io/hello-world/db/${tradingday}.json`, addTradingDay)
+  })
+}
+
+var addTradingDay = (jsondata) => {
+  ais[jsondata["date"]]["strategies"] = jsondata["strategies"]; 
+  ais[jsondata["date"]]["components"] = jsondata["components"]; 
 }
